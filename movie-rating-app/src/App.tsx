5c630485503dab11940110208d01105bd1569c4d
Gokselagan <gokselagan@outlook.com>
2024-07-19 23:37:16 +0300

    navbar added

--- movie-rating-app/src/App.tsx
+++ movie-rating-app/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import './App.css';
+import { Navbar } from './components/navbar.tsx';
 
 export const App = () => {
   return (
     <div className="app">
-
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<>Home Page</>} />
+          <Route path='/rated' element={<>Rated Page</>} />
+          <Route path='/auth' element={<>Auth Page</>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
