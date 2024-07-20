02acad08c0dcdb1d090c8e1e29a039ca699ba28e
Gokselagan <gokselagan@outlook.com>
2024-07-20 21:38:27 +0300

    api used in auth page

--- movie-rating-app/src/pages/auth/index.tsx
+++ movie-rating-app/src/pages/auth/index.tsx
@@ -1,9 +1,33 @@
 import React from "react";
 import {Grid, Header, Form, Segment, Button} from "semantic-ui-react";
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 export const Auth = () => {
 
-    const handleLogin = () => {}
+    const mutationLogin= async () => {
+        const res = await fetch ("https://api.themoviedb.org/3/authentication/guest_session/new ",
+
+        {
+            headers: {
+                Authorization: " Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNWEzNDYwM2FhODUzNTQxODhhNzQxNzFhMWFhYjBkMSIsIm5iZiI6MTcyMTQ5NTcwMy40NDMzNDgsInN1YiI6IjY2OWFiYzBlNjVjOGJhMWY3ZDViY2YyZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DENOy10SXoVXL-iYgQIzT_Xu46LQPLhxRWZwoDdWgOg"
+            },
+        });
+        return res.json();
+    }
+
+    const { data, mutate } = useMutation({mutationKey:["login"], mutationFn: mutationLogin})
+
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        await mutate();
+        if (data) {
+            localStorage.setItem("quest_session_id", data.guest_session_id)
+            navigate("/");
+        }
+        
+    }
 
     return(
         <Grid textAlign="center" verticalAlign="middle" style={{height: "100vh"}} >
