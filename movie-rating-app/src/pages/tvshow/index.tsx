43b5abbd9fdff0d7824ae76eeed518926235281f
Gokselagan <gokselagan@outlook.com>
2024-07-25 22:33:20 +0300

    tv show page main contents added

--- movie-rating-app/src/pages/tvshow/index.tsx
+++ movie-rating-app/src/pages/tvshow/index.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react"
 import { useParams } from "react-router-dom"
-import { Header, Loader, Segment } from "semantic-ui-react";
+import { Grid, GridColumn, Header, List, ListHeader, ListItem, Loader, Segment, Image } from "semantic-ui-react";
 import { fetchTvShowDetails } from "./query.ts";
 
 export const TvShow = () => {
@@ -18,9 +18,74 @@ export const TvShow = () => {
     }
 
     return(
-        <div style={{marginTop:"100px"}}>
+        <div style={{margin:"100px auto", maxWidth:"675px"}}>
             <Segment> 
-                <Header>{data.name}</Header>
+            <Header>{data.name}</Header>
+                <Grid columns={2} divided textAlign="left" style={{marginTop:"20px"}}>
+                    <Grid.Row>
+                        <GridColumn width={6}>
+                            <div
+                            style={{
+                                display:"flex",
+                                alignItems:"center",
+                                justifyContent:"center",
+                                height:"100%"
+                            }}
+                            >
+                                <Image src={`https://image.tmdb.org/t/p/original/${data.poster_path}`} size="medium" centered />
+                            </div>
+                        </GridColumn>
+
+                        <GridColumn width={10} style={{display:"flex"}}>
+                            <List style={{display:"flex", flexDirection:"column", justifyContent:"space-between"}}>
+                                <ListItem>
+                                    <ListHeader>Created By :</ListHeader>
+                                    {data.created_by.map((creator:any)=>creator.name).join(", ")}
+                                </ListItem>
+                                <ListItem>
+                                    <ListHeader>Episodes Run Time :</ListHeader>
+                                    {data.episode_run_time.join(", ")}
+                                </ListItem>
+                                <ListItem>
+                                    <ListHeader>Genres :</ListHeader>
+                                    {data.genres.map((genre: any)=>(
+                                        <ListItem key={genre.id}>{genre.name}</ListItem>
+                                    ))}
+                                </ListItem>
+                                <ListItem>
+                                    <ListHeader>First Air Date :</ListHeader>
+                                    {data.first_air_date}
+                                </ListItem>
+                                <ListItem>
+                                    <ListHeader>Networks :</ListHeader>
+                                    {data.networks.map((network: any)=>(
+                                        <Image 
+                                        key={network.id}
+                                        src={`https://image.tmdb.org/t/p/original/${network.logo_path}`}
+                                        size="small"
+                                        />
+                                    ))}
+                                </ListItem>
+                                <ListItem>
+                                    <ListHeader>Production Companies :</ListHeader>
+                                    {data.production_companies.map((company: any)=> company.name).join(", ")}
+                                </ListItem>
+                                <ListItem>
+                                    <ListHeader>Number Of Episodes :</ListHeader>
+                                    {data.number_of_episodes}
+                                </ListItem>
+                                <ListItem>
+                                    <ListHeader>Number Of seasons :</ListHeader>
+                                    {data.number_of_seasons}
+                                </ListItem>
+                                <ListItem>
+                                    <ListHeader>Language:</ListHeader>
+                                    {data.original_language}
+                                </ListItem>
+                            </List>
+                        </GridColumn>
+                    </Grid.Row>
+                </Grid>
             </Segment>
         </div>
     )
