81dd3cf3276b80e1547f404136e599b5d3eae78e
Gokselagan <gokselagan@outlook.com>
2024-07-30 17:46:28 +0300

    Move to another task, unresolved API issue in Rated page

--- movie-rating-app/src/pages/rated/index.tsx
+++ movie-rating-app/src/pages/rated/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Header, Menu, MenuItem, Segment } from "semantic-ui-react";
+import { Container, Header, Loader, Menu, MenuItem, Segment } from "semantic-ui-react";
 import { DisplayType } from "../homepage/index.tsx";
 import { fetchRatedMovieDetails, fetchRatedTvShowsDetails } from "./query.ts";
 import { useQuery } from "@tanstack/react-query";
@@ -7,30 +7,49 @@ import { ShowCase } from "../homepage/showcase.tsx";
 
 export const Rated = () => {
 
-    const [activeTabs,setActiveTabs] = useState<DisplayType>(DisplayType.Movies);
+    const [activeTabs, setActiveTabs] = useState<DisplayType>(DisplayType.Movies);
 
-    const {data : ratedMovies, isLoading: isLoadingRatedMovies} = useQuery({queryKey:["ratedMovies"], queryFn : fetchRatedMovieDetails})
+    const { data: ratedMovies, isLoading: isLoadingRatedMovies } = useQuery({ queryKey: ["ratedMovies"], queryFn: fetchRatedMovieDetails })
 
-    const {data : ratedTvShows, isLoading: isLoadingRatedTvShows} = useQuery({queryKey:["ratedTvShows"], queryFn : fetchRatedTvShowsDetails})
-    return(
-        <Container style={{marginTop:"60px"}}>
+    const { data: ratedTvShows, isLoading: isLoadingRatedTvShows } = useQuery({ queryKey: ["ratedTvShows"], queryFn: fetchRatedTvShowsDetails })
+
+    console.log("data", ratedMovies);
+    return (
+        <Container style={{ marginTop: "60px" }}>
             <Menu pointing secondary>
-                <MenuItem name="Movies" active={activeTabs === DisplayType.Movies} onClick={()=>setActiveTabs(DisplayType.Movies)}></MenuItem>
-                <MenuItem name="Tv Show" active={activeTabs === DisplayType.TvShows} onClick={()=>setActiveTabs(DisplayType.TvShows)}></MenuItem>
+                <MenuItem name="Movies" active={activeTabs === DisplayType.Movies} onClick={() => setActiveTabs(DisplayType.Movies)}></MenuItem>
+                <MenuItem name="Tv Show" active={activeTabs === DisplayType.TvShows} onClick={() => setActiveTabs(DisplayType.TvShows)}></MenuItem>
             </Menu>
 
             <Segment>
                 {activeTabs === DisplayType.Movies ? (
                     <div>
                         <Header as={"h2"}>Rated Movies</Header>
-                        <ShowCase  data={ratedMovies?.results} displayType={DisplayType.Movies}/>
+
+                        {!isLoadingRatedMovies & ratedMovies ? (
+                            < ShowCase data={ratedMovies?.results || []} displayType={DisplayType.Movies} />
+                        ) : (
+                            <>
+                                <p>Rated Movies Loading...</p>
+                                <Loader active inline="centered" />
+                            </>
+                        )}
+
                     </div>
                 ) : (
                     <div>
                         <Header as={"h2"}>Tv Shows</Header>
-                        <ShowCase  data={ratedTvShows?.results} displayType={DisplayType.TvShows}/>
+                        {!isLoadingRatedTvShows & ratedTvShows ? (
+                            <ShowCase data={ratedTvShows?.results || []} displayType={DisplayType.TvShows} />
+                        ) : (
+                            <>
+                                <p>Rated Tv Shows Loading...</p>
+                                <Loader active inline="centered" />
+                            </>
+                        )}
+
                     </div>
-                ) }
+                )}
             </Segment>
         </Container>
     )
--- movie-rating-app/src/pages/rated/query.ts
+++ movie-rating-app/src/pages/rated/query.ts
@@ -2,6 +2,7 @@ export const fetchRatedMovieDetails = async () => {
     const res = await fetch(`https://api.themoviedb.org/3/guest_session/${localStorage.getItem("guest_session_id")}/rated/movies?language=en-US&page=1&sort_by=created_at.asc&api_key=${process.env.REACT_APP_API_KEY}`,
     );
     const data = await res.json();
+    console.log("movie data", data);
     return data;
 }
 
@@ -9,5 +10,6 @@ export const fetchRatedTvShowsDetails = async () => {
     const res = await fetch(`https://api.themoviedb.org/3/guest_session/${localStorage.getItem("guest_session_id")}/rated/tv?language=en-US&page=1&sort_by=created_at.asc&api_key=${process.env.REACT_APP_API_KEY}`,
     );
     const data = await res.json();
+    console.log("tv show data", data);
     return data;
 }
